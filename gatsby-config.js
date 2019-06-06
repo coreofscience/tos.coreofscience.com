d471c3dd0022308b7cee5aab0dfc1fc301ccd5df
jdalzatec <jdalzatec@unal.edu.co>
2019-06-05 21:32:38 -0500

    Update colors and fonts

--- gatsby-config.js
+++ gatsby-config.js
@@ -22,7 +22,7 @@ module.exports = {
         short_name: `tosoffline`,
         start_url: `/`,
         background_color: `#663399`,
-        theme_color: `#663399`,
+        theme_color: `#000000`,
         display: `minimal-ui`,
         icon: `src/images/icon.png`, // This path is relative to the root of the site.
       },
