5e217c3dfef3a09bd119bf39d87e2f7125b12dac
jdalzatec <jdalzatec@unal.edu.co>
2019-06-06 17:10:43 -0500

    Remove import line from index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 
 import Layout from "../components/layout"
-import IssueImage from "../components/issueImage"
 import SEO from "../components/seo"
 
 const IndexPage = () => (
