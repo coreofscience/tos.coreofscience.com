a359ee57d6a5c1a06991cdbb907f8ffaac40e511
jdalzatec <jdalzatec@unal.edu.co>
2019-06-06 17:21:52 -0500

    Change colors (again)

--- src/components/header.js
+++ src/components/header.js
@@ -5,7 +5,7 @@ import React from "react"
 const Header = ({ siteTitle }) => (
   <header
     style={{
-      background: `#824d1e`,
+      background: `#235F13`,
       marginBottom: `1.45rem`,
     }}
   >
@@ -20,7 +20,7 @@ const Header = ({ siteTitle }) => (
         <Link
           to="/"
           style={{
-            color: `#f2d6aa`,
+            color: `#F2D6AA`,
             textDecoration: `none`,
           }}
         >
