f8991e90fd73db7f4832223fb32979d8534b16fb
Oscar Arbeláez <odarbelaeze@gmail.com>
2019-06-07 08:09:46 -0500

    Give final touches to the text

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,17 +9,16 @@ const IndexPage = () => (
     <h1>Tree of Science is temporary offline</h1>
     <p>
       Tree of Science is going to be offline for a while. We the ToS team, now
-      known as Core of Science, and Universidad Nacional de Colombia (sede
-      Manizales) are working on some intellectual property related issues.
-      Although we are working hard and towards a common goal the issues might
-      take some time to be resolved.
+      known as Core of Science, and Universidad Nacional de Colombia are working
+      on some intellectual property related issues. Although we are working hard
+      and towards a common goal the issues might take some time to resolve.
     </p>
     <h2>When can we expect to have tree of science online again?</h2>
     <p>
       Core of Science, as an organization, is working on a more open, performant
-      and flexible version of the ToS website you know and love. And
-      furthermore, we'll make it compliant with the latest privacy regulations
-      such as GDPR in Europe and "Habeas data" in Colombia.
+      and flexible version of the ToS website you know and love. Furthermore,
+      we'll make it compliant with the latest privacy regulations such as GDPR
+      in Europe and "Habeas data" in Colombia.
     </p>
     <h2>What can we do to help?</h2>
     <p>
