766b86b8e562d275bd100c727b824290e6e0f581
Oscar Arbeláez <odarbelaeze@gmail.com>
2020-10-07 17:21:53 -0500

    Add adroll pixel thing

--- src/components/layout.js
+++ src/components/layout.js
@@ -49,6 +49,14 @@ const Layout = ({ children }) => (
             </a>
           </footer>
         </div>
+        <script
+          type="text/javascript"
+          dangerouslySetInnerHTML={{
+            __html: `
+            adroll_adv_id = "W4X6GCU4RZENHET6NWIISX"; adroll_pix_id = "DGYWWU6WRVHYZKPV66RLXX"; adroll_version = "2.0";  (function(w, d, e, o, a) { w.__adroll_loaded = true; w.adroll = w.adroll || []; w.adroll.f = [ 'setProperties', 'identify', 'track' ]; var roundtripUrl = "https://s.adroll.com/j/" + adroll_adv_id + "/roundtrip.js"; for (a = 0; a < w.adroll.f.length; a++) { w.adroll[w.adroll.f[a]] = w.adroll[w.adroll.f[a]] || (function(n) { return function() { w.adroll.push([ n, arguments ]) } })(w.adroll.f[a]) }  e = d.createElement('script'); o = d.getElementsByTagName('script')[0]; e.async = 1; e.src = roundtripUrl; o.parentNode.insertBefore(e, o); })(window, document); adroll.track("pageView");
+          `,
+          }}
+        />
       </>
     )}
   />
